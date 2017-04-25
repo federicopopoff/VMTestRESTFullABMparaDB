5a8ac483cc4682d01bc2430c7fdf8956ade6732c
Hans Aparicio <hansaparicio@gmail.com>
2017-04-25 12:08:17 -0300

    Creacion de cliente

---
+++ Evaluacion2/Client/Controller/controllerFront.js
@@ -0,0 +1,30 @@
+﻿//creacion de controlador
+angular
+    .module('myApp')
+    .controller('myCtrl', myCtrl);
+
+//inyeccion
+myCtrl.$inject = ['$scope', 'apiservice']
+
+//declaracion de metodos y propiedades
+
+function myCtrl($scope, apiservice) {
+    $scope.getAllUsers = function () {
+        apiservice.getAllUsers();
+    };
+    $scope.getUser = function (user) {
+        apiservice.getUser(user);
+    };
+    $scope.deleteUser = function (user) {
+        apiservice.deleteUser(user);
+    };
+    $scope.updateUser = function (user, nombre, apellido, email, password) {
+        apiservice.updateUser(user, nombre, apellido, email, password);
+    };
+    $scope.createUser = function (user, nombre, apellido, email, password) {
+        apiservice.createUser(user, nombre, apellido, email, password);
+    };
+    $scope.prueba = function (texto){
+        console.log(texto);
+    }
+}
---
+++ Evaluacion2/Client/Services/serivicioFront.js
@@ -0,0 +1,44 @@
+﻿//creacion de servicio
+angular
+    .module('myApp')
+    .factory('apiservice', apiservice);
+
+//inyeccion
+apiservice.$inject = ['$http'];
+
+function apiservice($http) {
+    return {
+        getAllUsers: function () {
+            $http.get("../usuarios/users/")
+                .then(function (response) {
+                    console.log(response.data);
+                });
+        },
+        getUser: function (user) {
+            $http.get("../usuarios/users/"+user)
+                .then(function (response) {
+                    console.log(response.data);
+                });
+        },
+        deleteUser: function (user) {
+            $http.delete("../usuarios/users/" + user)
+                .then(function (response) {
+                    console.log(response.data);
+                });
+        },
+        updateUser: function (user,nombre,apellido,email,password) {
+            data = '{"Id":'+user+',"nombre":"'+nombre+'","apellido":"'+apellido+'","email":"'+email+'","password":"'+password+'"}';
+            $http.put("../usuarios/users/" + user)
+                .then(function (response) {
+                    console.log(response.data);
+                });
+        },
+        createUser: function (user,nombre,apellido,email,password) {
+            data = '{"Id":'+user+',"nombre":"'+nombre+'","apellido":"'+apellido+'","email":"'+email+'","password":"'+password+'"}';
+            $http.post("../usuarios/users/" + user)
+                .then(function (response) {
+                    console.log(response.data);
+                });
+        }
+    }
+}
